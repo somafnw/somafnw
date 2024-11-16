55ad48f609aee29c8c70e584cac8504c8d0179a1
somafnw <soma.fnw@hotmail.com>
2024-11-16 14:14:42 +0200

    Add files via upload

---
+++ assets/js/type.js
@@ -0,0 +1,13 @@
+window.ityped.init(document.querySelector('.ityped'),{
+    strings: ['του Εαυτού σας'],
+    typeSpeed: 250,
+    backSpeed: 200,
+    loop: true
+})
+
+window.ityped.init(document.querySelector('.ityped2'),{
+    strings: ['που Κρύβετε'],
+    typeSpeed: 250,
+    backSpeed: 200,
+    loop: true
+})
